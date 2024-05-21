3710235ef54498f9f149ccbf970048981668b40e
sypark <upperrr@gmail.com>
2024-05-22 08:27:55 +0900

    :tada:init: initialize

---
+++ src/main/java/so/jejuschrist/JejuschristApplication.java
@@ -0,0 +1,13 @@
+package so.jejuschrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuschristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JejuschristApplication.class, args);
+    }
+
+}
---
+++ src/test/java/so/jejuschrist/JejuschristApplicationTests.java
@@ -0,0 +1,13 @@
+package so.jejuschrist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuschristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
