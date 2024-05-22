2130a69f57cf1c92e704c07efcf03c31eac7ce56
sypark <upperrr@gmail.com>
2024-05-22 14:34:19 +0900

    :rewind:rollback: rollback webToB

--- src/main/java/so/jejuschrist/JejuschristApplication.java
+++ src/main/java/so/jejuschrist/JejuschristApplication.java
@@ -2,19 +2,12 @@ package so.jejuschrist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JejuschristApplication extends SpringBootServletInitializer {
+public class JejuschristApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JejuschristApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(JejuschristApplication.class);
     }
 
 }
